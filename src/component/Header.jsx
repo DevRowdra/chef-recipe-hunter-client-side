39e8cdeaff4c24a9ad0ea39a1cbe4d9ee04565c5
DevRowdra <121636096+DevRowdra@users.noreply.github.com>
2023-05-02 09:35:59 +0600

    add navLink for active link

--- src/component/Header.jsx
+++ src/component/Header.jsx
@@ -1,57 +1,85 @@
 import React, { useContext } from 'react';
-import { Link } from 'react-router-dom';
+import { Link, NavLink } from 'react-router-dom';
 import { AuthContext } from './Provider/AuthProvider';
 
 const Header = () => {
+  const { user, logoutUser } = useContext(AuthContext);
 
+  const handleLogout = () => {
+    logoutUser()
+      .then(() => {
+        console.log('logout com');
+      })
+      .catch((error) => {
+        console.log(error.message);
+      });
+  };
 
-const{user,logoutUser}=useContext(AuthContext)
-
-const handleLogout=()=>{
-logoutUser()
-.then(()=>{
-  console.log('logout com')
-})
-.catch((error)=>{
-  console.log(error.message)
-})
-}
-
-
-    return (
-        <div >
+  return (
+    <div>
       <div className="navbar bg-transparent">
         <div className="flex-1 ">
-        <Link to={'/'}>BongDine</Link>
+          <Link to={'/'}>BongDine</Link>
         </div>
-        
+
         <div className="flex-none">
           <ul className="menu menu-horizontal px-1">
             <li>
-              <Link to={'/'}>Home</Link>
+            <NavLink
+                to="/"
+                className={({ isActive, isPending }) =>
+                  isPending ? 'pending' : isActive ? 'active' : ''
+                }
+              >
+                Home
+              </NavLink>
             </li>
-            <li tabIndex={0}>
-              <Link to={'/blog'}>Blog</Link>
+            <li >
+            <NavLink
+                to="/blog"
+                className={({ isActive, isPending }) =>
+                  isPending ? 'pending' : isActive ? 'active' : ''
+                }
+              >
+                Blog
+              </NavLink>
+            </li>
+
+            <li>
+            <NavLink
+                to="/register"
+                className={({ isActive, isPending }) =>
+                  isPending ? 'pending' : isActive ? 'active' : ''
+                }
+              >
+                Register
+              </NavLink>
             </li>
             
-            <li>
-              <Link to='/register'>Register</Link>
-            </li>
-           {user &&  <li>
-              <p className='bg-gray-400 text-slate-50' >{user.displayName}</p>
-            </li>}
+            {user && (
+              <li>
+                <p className="bg-gray-400 text-slate-50">{user.displayName}</p>
+              </li>
+            )}
           </ul>
-          
-         {user ? <button onClick={handleLogout} className="btn btn-active btn-warning rounded-md">
-            LogOut
-          </button> :
-          <button className="btn btn-active btn-warning rounded-md">
-           <Link to='/login'>Login</Link>
-          </button>}
+
+          {user ? (
+            <Link
+              onClick={handleLogout}
+              
+              className="btn btn-active btn-warning rounded-md "
+            >
+              LogOut
+            </Link>
+          ) : (
+            <button className="btn btn-active btn-warning rounded-md">
+              <Link to="/login">Login</Link>
+            </button>
+          )}
         </div>
       </div>
     </div>
-    );
+  );
 };
 
 export default Header;
